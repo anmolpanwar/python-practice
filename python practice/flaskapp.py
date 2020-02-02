df557b4c6cf74764b39f1267d8306dd67b3218f8
Anmol Panwar <anmolpanwar8@gmail.com>
2020-02-02 21:29:25 +0530

    pycharm unix epoch commit1

--- python practice/flaskapp.py
+++ python practice/flaskapp.py
@@ -1,1 +1,9 @@
 from flask import *
+app = Flask(__name__)
+
+@app.route('/home')
+def func():
+    return render_template('first.html')
+
+if __name__=='__main__':
+    app.run(host='http://127.0.0.1',port = 5000, debug=True)
